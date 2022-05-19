9d8b444e981af0a98d72ac95231accee92bb7308
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-19 12:50:31 +0300

    Added the onload eventlistener

---
+++ game.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boundaries = document.querySelectorAll('.boundary')
+    const start = document.querySelector('#start')
+    const end = document.querySelector('#end')
+    const status = document.querySelector('#status')
+
+    let losing = () => {
+        status.innerText = 'You Lost'
+        status.style.color = 'red'
+        for (let boundary of boundaries) {
+            boundary.style.backgroundColor = 'red'
+        }
+    }
+
+    let winning = () => {
+        status.innerText = 'You Won'
+        status.style.color = 'Green'
+        for (let boundary of boundaries) {
+            boundary.removeEventListener('pointerover', losing, { once: true })
+        }
+    }
+
+    start.addEventListener('pointerover', () => {
+        for (let boundary of boundaries) {
+            boundary.addEventListener('pointerover', losing, { once: true })
+            end.addEventListener('pointerover', winning, { once: true })
+        }
+    }, { once: true })
+})
+
